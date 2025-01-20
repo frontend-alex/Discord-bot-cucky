7681193da687af255c417cd4a737f30b042e0465
I554344 <alexmmech@gmail.com>
2025-01-20 03:14:46 +0100

    sdasd

--- server/src/constants/Data.ts
+++ server/src/constants/Data.ts
@@ -2,4 +2,4 @@ import { GuildMember } from "discord.js";
 
 export const randomWords = ["Are da obadite nqkakwi zamulchaanki da vi zaeba", "Mluk da vi eba", "Hlebistosi mluk"];
 
-export const greetingMessage = (member: GuildMember) => `Welcome to the server, ${member.user.username}! 👋 We're happy to have you here!`;
+export const greetingMessage = (member: GuildMember) => `Evala she se setish, **${member.user.username}**! 👋 Ne se prai na gotin`;
